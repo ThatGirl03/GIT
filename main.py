6ea69aedcac1f01bab7ca4563c78868028829905
ThatGirl03 <kwaneleayandamazibuko@gmail.com>
2024-11-09 20:53:04 +0200

    port 2

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Get the port from the environment, default to 5000 for local testing
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
