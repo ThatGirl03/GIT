895f735bf61a377ecca85ff1acc25b82c7c89a9c
ThatGirl03 <kwaneleayandamazibuko@gmail.com>
2024-11-09 20:49:19 +0200

    port

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from website import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the port from the environment, default to 5000 for local testing
+    app.run(host='0.0.0.0', port=port, debug=True)
